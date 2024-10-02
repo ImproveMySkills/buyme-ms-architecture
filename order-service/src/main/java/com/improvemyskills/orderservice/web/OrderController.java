e369a150efbe1901cf1dbf4adfac727de35ac606
ahmadou.aboubakar <ahmadou.aboubakar@capgemini.com>
2024-10-02 12:56:31 +0200

    feat: adding feign client

--- order-service/src/main/java/com/improvemyskills/orderservice/web/OrderController.java
+++ order-service/src/main/java/com/improvemyskills/orderservice/web/OrderController.java
@@ -2,14 +2,14 @@ package com.improvemyskills.orderservice.web;
 
 import com.improvemyskills.orderservice.clients.CustomerRestClient;
 import com.improvemyskills.orderservice.clients.ProductRestClient;
+import com.improvemyskills.orderservice.entity.OrderPurshase;
 import com.improvemyskills.orderservice.models.Customer;
 import com.improvemyskills.orderservice.models.Product;
+import com.improvemyskills.orderservice.service.OrderService;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.List;
@@ -19,10 +19,12 @@ import java.util.List;
 public class OrderController {
     CustomerRestClient customerRestClient;
     ProductRestClient productRestClient;
+    OrderService orderService;
 
-    public OrderController(CustomerRestClient customerRestClient, ProductRestClient productRestClient) {
+    public OrderController(CustomerRestClient customerRestClient, ProductRestClient productRestClient, OrderService orderService) {
         this.customerRestClient = customerRestClient;
         this.productRestClient = productRestClient;
+        this.orderService = orderService;
     }
 
     @GetMapping("/customers")
@@ -33,6 +35,21 @@ public class OrderController {
     @GetMapping("/products")
     public ResponseEntity<Collection<Product>> getProductRestClient() {
         return productRestClient.getAll();
+    }
+
+    @PostMapping("/orders")
+    public ResponseEntity<OrderPurshase> placeAnOrder(@RequestBody OrderPurshase orderPurshase){
+        return null;
+    }
+
+    @GetMapping("/orders")
+    public ResponseEntity<Collection<OrderPurshase>> getOrders(){
+        /*PagedModel<Customer> customers = customerRestClient.getAllCustomers();
+        Collection<Product> products = productRestClient.getAll();
+        Collection<OrderPurshase> orders = orderService.getAllOrders().stream()
+                .forEach(orderPurshase -> {
+                });*/
+        return null;
     }
 
 
